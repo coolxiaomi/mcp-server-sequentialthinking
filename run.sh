bc7550721d153a0e5a2dda5ea2b81399436c3a22
amitwu <coolxiaomi@163.com>
2025-05-17 15:59:54 +0800

    增加docker run 的sh

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
 docker build -t registry.cn-hangzhou.aliyuncs.com/zhyx-app/mcp-sequentialthinking-sse .
+
+docker run --restart=always -d \
+--name mcp-sequentialthinking-sse \
+-e SSE_ADDR=127.0.0.1:3000 \
+-p 9005:3000 \
+registry.cn-hangzhou.aliyuncs.com/zhyx-app/mcp-sequentialthinking-sse
